e3df2d5c3c9ad4d153767b92672e593367ddf292
squest <sabdaps@gmail.com>
2015-12-13 04:24:44 +0700

    cleaning up things

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "https://github.com/squest"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.taoensso/timbre "4.1.4"]]
   :repositories [["releases" {:url   "http://clojars.org/repo"
                               :creds :gpg}]])
--- test/pertamax/test/physics.clj
+++
@@ -1,8 +0,0 @@
-(ns pertamax.test.physics
-  (:require
-    [clojure.test :refer :all]
-    [pertamax.physics :refer :all]))
-
-(deftest early-test
-  (testing "early"
-    (is (= 2 (+ 1 1)))))
