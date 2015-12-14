4c313eb836c45f94cdaba5b13bc6dd7eec05cdb4
squest <sabdaps@gmail.com>
2015-12-15 03:56:53 +0700

    another attempt to fix the test

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "https://github.com/squest"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.taoensso/timbre "4.1.4"]]
+                 [com.taoensso/timbre "4.1.4"]
+                 [gorilla-plot "0.1.3"]]
   :plugins [[lein-gorilla "0.3.5"]]
   :repositories [["releases" {:url   "http://clojars.org/repo"
                               :creds :gpg}]])
