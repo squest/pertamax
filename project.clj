72a51b8fe55ff48275a4e134fb57fd8fae460898
squest <sabdaps@gmail.com>
2015-12-15 06:25:00 +0700

    bump to 1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zenedu/pertamax "0.1.6"
+(defproject zenedu/pertamax "0.1.7"
   :description "Functions to test math/physics/chem problems"
   :url "https://github.com/squest"
   :license {:name "Eclipse Public License"
